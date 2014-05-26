392d7f52b6595e2de856416db7b101b63e231b0c
pcresolver <steve@pcresolver.es>
2014-05-26 13:53:00 +0100

    small edit change at 12:22 closing pycharm without pushing

---
+++ calls.py
@@ -0,0 +1,24 @@
+__author__ = 'Steve'
+def total_cost(calls):
+    print(calls)
+    print(calls[0])
+    for day in len(calls):
+        print("day ", day, calls[int(day)])
+
+    return 0
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert total_cost(("2014-01-01 01:12:13 181",
+                       "2014-01-02 20:11:10 600",
+                       "2014-01-03 01:12:13 6009",
+                       "2014-01-03 12:13:55 200")) == 124, "Base example"
+    assert total_cost(("2014-02-05 01:00:00 1",
+                       "2014-02-05 02:00:00 1",
+                       "2014-02-05 03:00:00 1",
+                       "2014-02-05 04:00:00 1")) == 4, "Short calls but money..."
+    assert total_cost(("2014-02-05 01:00:00 60",
+                       "2014-02-05 02:00:00 60",
+                       "2014-02-05 03:00:00 60",
+                       "2014-02-05 04:00:00 6000")) == 106, "Precise calls"
